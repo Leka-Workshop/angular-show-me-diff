1c615918e19d6c2aca744c9df990a9b01be0cae3
mirzaleka <mirza_376@hotmail.com>
2023-11-27 21:30:39 +0100

    created diff, union, intersection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Change, diffWordsWithSpace } from 'diff';
+
+enum FormFields {
+  Original = 'original',
+  Updated = 'updated',
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'show-me-diff';
+export class AppComponent implements OnInit {
+  title = 'Show Me Diff';
+
+  union!: string;
+  differences!: string;
+  intersection!: string;
+
+  compareForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.setupForm();
+  }
+
+  private setupForm(): void {
+    this.compareForm = this.fb.group({
+      [FormFields.Original]: [''],
+      [FormFields.Updated]: [''],
+    });
+  }
+
+  onSubmit(): void {
+    const { original, updated } = this.compareForm.value;
+
+    // Union
+    const changes = diffWordsWithSpace(original, updated, { ignoreCase: true });
+    this.union = changes.map((part: Change) => part.value).join(' ');
+
+    // Differences
+    const addedChanges = changes.filter((part) => part.added);
+    this.differences = addedChanges.map((part) => part.value).join(' ');
+
+    // Intersection
+    const originalWords = original.split(/\s+/);
+    const updatedWords = updated.split(/\s+/);
+
+    this.intersection = originalWords
+      .filter((word: string) => updatedWords.includes(word))
+      .join(' ');
+  }
 }
