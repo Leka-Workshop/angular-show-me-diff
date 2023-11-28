4834e7b35560fb378d88fe583a6352a8db9510a1
mirzaleka <mirza_376@hotmail.com>
2023-11-28 02:33:41 +0100

    set text highlghting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Change, diffWordsWithSpace } from 'diff';
+import { Subject, takeUntil } from 'rxjs';
 
 enum FormFields {
   Original = 'original',
   Updated = 'updated',
+  Language = 'language'
 }
 
 @Component({
@@ -12,27 +15,54 @@ enum FormFields {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+  private unsubscribe$ = new Subject<void>();
+
   title = 'Show Me Diff';
 
   union!: string;
   differences!: string;
   intersection!: string;
 
+  formatLanguage = 'typescript';
   compareForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     this.setupForm();
+    // this.listenOnLanguageChanges();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   private setupForm(): void {
     this.compareForm = this.fb.group({
       [FormFields.Original]: [''],
       [FormFields.Updated]: [''],
+      // [FormFields.Language]: ['1']
     });
   }
+
+  setLanguage(lng: string) {
+    console.log(lng);
+    this.formatLanguage = lng;
+  }
+
+  // private listenOnLanguageChanges() {
+  //   this.compareForm.get(FormFields.Language)?.valueChanges
+  //   .pipe(
+  //     takeUntil(this.unsubscribe$)
+  //   )
+  //   .subscribe((newSelection: string) => {
+  //     this.formatLanguage = newSelection;
+  //     this.cdr.detectChanges(); // Manually trigger change detection
+
+  //   })
+  // }
 
   onSubmit(): void {
     const { original, updated } = this.compareForm.value;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
+import 'highlight.js/lib/languages/typescript';
+import 'highlight.js/lib/languages/javascript';
+
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HighlightModule, HighlightOptions, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 
 import { AppComponent } from './app.component';
 
@@ -10,9 +14,27 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HighlightModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: <HighlightOptions>{
+        lineNumbers: true,
+        coreLibraryLoader: () => import('highlight.js/lib/core'),
+        lineNumbersLoader: () => import('ngx-highlightjs/line-numbers'),
+        themePath: 'node_modules/highlight.js/styles/github.css',
+        languages: {
+          typescript: () => import('highlight.js/lib/languages/typescript'),
+          css: () => import('highlight.js/lib/languages/css'),
+          xml: () => import('highlight.js/lib/languages/xml'),
+          sql: () => import('highlight.js/lib/languages/sql'),
+          vbnet: () => import('highlight.js/lib/languages/vbnet'),
+        },
+      },
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
